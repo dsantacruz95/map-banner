6e2008776bb56a62eaa99161637670db5b19cdd0
usr_dsantacruz <dsantacruz@innovaschools.edu.pe>
2024-07-04 13:39:27 -0500

    conectar con sala por guid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,18 +15,34 @@ import { io } from 'socket.io-client';
 export class AppComponent {
   title = 'map-banner';
   listMaps: Map[] = [];
+  errorMessage = '';
   private socket: any;
+  private urlParams = new URLSearchParams(window.location.search);
+  private matchParam = this.urlParams.get('match');
 
   constructor() { }
 
   ngOnInit() {
+    if (!this.matchParam) {
+      this.errorMessage = 'Invalid URL or the match code is not present 🤦';
+      return;
+    }
+
     this.socket = io('http://192.168.18.235:3000');
+    this.socket.on('request-guid', () => {
+      this.socket.emit('provide-guid', this.matchParam);
+    });
     this.socket.on('maps', (maps: Map[]) => {
       this.listMaps = maps;
+      if (!this.listMaps || this.listMaps.length === 0) {
+        this.errorMessage = 'No maps found for this match 😺';
+      }
     });
   }
 
   disableMap(map: Map) {
-    this.socket.emit('disable-map', map.id);
+    if (map.disabled || map.winner)
+      return;
+    this.socket.emit('disable-map', {mapId: map.id, matchId: this.matchParam});
   }
 }
--- src/app/models/map.ts
+++ src/app/models/map.ts
@@ -1,10 +1,10 @@
 export class Map {
-    id: string;
+    id: number;
     name: string;
     disabled: boolean;
     winner: boolean;
 
-    constructor(id: string, name: string, disabled: boolean = false, winner: boolean = false) {
+    constructor(id: number, name: string, disabled: boolean = false, winner: boolean = false) {
         this.id = id;
         this.name = name;
         this.disabled = disabled;
